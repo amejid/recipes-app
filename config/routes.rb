4d516b65087e8d4592d89b312d1523eb3ef61a5c
Htet Naing <htetnaing0814@gmail.com>
2022-11-08 11:30:11 +0630

    added a form to add Food

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :recipes, except: [:edit, :update]
-  resources :foods, only: :index
-
   root to: 'recipes#index'
+
+  resources :recipes, except: [:edit, :update]
+  resources :foods, except: [:edit, :update]
+
   get '/public_recipes', to: 'recipes#public'
   get '/general_shopping_list', to: 'recipes#general'
 end
