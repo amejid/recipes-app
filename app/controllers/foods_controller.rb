7f3ef73a5d7d3cfe9c078acf4d27f637744ee1d4
Htet Naing <htetnaing0814@gmail.com>
2022-11-08 12:42:22 +0630

    added food create method

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,8 +8,27 @@ class FoodsController < ApplicationController
   end
 
   def create
-    @food = Food.new(food_params)
+    food = current_user.foods.new(food_params)
+
+    if food.save
+      redirect_to foods_path, notice: "New Food was successfully added."
+    else
+      flash[:alert] = "New Food adding Failed. Please try again."
+    end
   end
 
-  def destroy; end
+  def destroy
+    food = Food.find(params[:id])
+
+    if food.destroy
+      flash[:notice] = "Food was successfully deleted."
+    else
+      flash[:alert] = "Food deleting Failed. Please try again."
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :quantity, :measurement_unit, :price)
+  end
 end
